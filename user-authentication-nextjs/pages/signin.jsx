81b40788c2a49d3ce9d9eba05b9ab3b003d05d71
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2022-09-10 01:20:10 +0530

    Update signin.jsx

--- user-authentication-nextjs/pages/signin.jsx
+++ user-authentication-nextjs/pages/signin.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Signin = () => {
   return (
-    <div>I am signin page</div>
-  )
-}
+    <div className="">
+      <div className="form-data ">
+        <form action="/send-data-here" method="post">
+          <label for="first">First name:</label>
+          <input type="text" id="first" name="first"  className="border-solid border-2 border-black"/>
+          <label for="last">Last name:</label>
+          <input type="text" id="last" name="last"className="border-solid border-2 border-black" />
+          <button type="submit" className="border-solid border-2 border-black bg-blue-500 mx-10">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Signin
+export default Signin;
+
